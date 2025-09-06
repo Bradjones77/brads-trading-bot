dc64d27fe4273358083cff4499ab4241d9616cab
Bradjones77 <bradleyjones77@hotmail.com>
2025-09-06 20:21:45 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # main.py
 from signal_generator import run_signals
 
-# Example list of coins you want to track
-coins = [
-    "BTC", "ETH", "USDT", "BNB", "DOGE", "ADA", "SOL", "MATIC", "XRP"
-]
+coins = ["BTC", "ETH", "DOGE", "ADA", "SOL"]
 
 if __name__ == "__main__":
     run_signals(coins)
